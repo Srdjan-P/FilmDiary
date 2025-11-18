55d763afe906958bf6e74a9c7789c24606c74a49
Srdjan-P <srdjan.potrebic@gmail.com>
2025-11-18 18:39:22 +0100

    Implemented sorting functionality by user rating for the watched movies list

--- src/components/WatchedList.jsx
+++ src/components/WatchedList.jsx
@@ -3,6 +3,11 @@ import Loader from "./Loader";
 import NoResults from "./NoResults";
 import SearchResults from "./SearchResults";
 import WatchedMovie from "./WatchedMovie";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useState } from "react";
 
 export default function WatchedList({
   watched,
@@ -12,6 +17,7 @@ export default function WatchedList({
   localQuery,
   onSelectMovie,
 }) {
+  const [sortBy, setSortBy] = useState("default");
   const filteredData = watched.filter((item) => {
     if (!localQuery || localQuery === "") {
       return item;
@@ -20,6 +26,23 @@ export default function WatchedList({
       const title = item.title || item.Title || "";
 
       return title?.toLowerCase().includes(searchTerm);
+    }
+  });
+
+  const sortedData = [...filteredData].sort((a, b) => {
+    const lowRating = parseFloat(a.userRating);
+    const highRating = parseFloat(b.userRating);
+
+    if (sortBy === "high") {
+      return highRating - lowRating;
+    }
+
+    if (sortBy === "low") {
+      return lowRating - highRating;
+    }
+
+    if (sortBy === "default") {
+      return filteredData;
     }
   });
 
@@ -39,9 +62,29 @@ export default function WatchedList({
               <Button onClick={() => setConfirmation(true)}>CLEAR LIST</Button>
             )}
           </SearchResults>
+
+          {watched.length > 0 && (
+            <div className="select">
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="sort-by">Sort by Rating</InputLabel>
+                <Select
+                  labelId="sort-by"
+                  id="sort-by"
+                  value={sortBy}
+                  label="Sort by Rating"
+                  onChange={(e) => setSortBy(e.target.value)}
+                >
+                  <MenuItem value="default">Default</MenuItem>
+                  <MenuItem value="high">High to Low</MenuItem>
+                  <MenuItem value="low">Low to High</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+          )}
+
           <div className="watched-list">
             <ul>
-              {filteredData.map((movie) => (
+              {sortedData.map((movie) => (
                 <WatchedMovie
                   movie={movie}
                   key={movie.imdbID}
